729e5f3a65ef6b827147c2970231f9e14121ef4a
stajicvladimir <vstajic@gmail.com>
2018-07-27 14:20:26 +0200

    no idea what and where I'm commiting

--- src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -5,13 +5,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class AnnotationDemoApp {
 
 	public static void main(String[] args) {
+		
 		ClassPathXmlApplicationContext context =
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		Coach tennisCoach = context.getBean("thatSillyCoach", Coach.class);
+		Coach tennisCoach = context.getBean("tennisCoach", Coach.class);
 		
 		System.out.println(tennisCoach.getDailyWorkout());
 		
+		System.out.println(tennisCoach.getDailyfortune());
 		context.close();
 	}
 
